7af24dd27faecc802b20e538456b99af17164181
krijnen <hielke.krijnen@hccnet.nl>
2016-11-28 11:56:37 +0100

    added comments to the problem class

--- problem.py
+++ problem.py
@@ -4,25 +4,22 @@ import numpy as np
 
 class Problem:
   def __init__(s, types, landings ):
-    s.landings = landings
-    s.types = types     #landings
-#    s.to_types = ["T_m", "T_h"]       #takeoffs
+    s.landings = landings             # nr of landings
+    s.types = types                   # aircraft types
+#    s.to_types = ["T_m", "T_h"]      # takeoffs
     s.events = s.genevents()
     s.costs = s.gencosts()    
     s.problem = LpProblem("Runway Optimization", LpMinimize)        #Minimizing Problem
     s.variables = LpVariable.dicts("Variables", s.events, cat='Integer', lowBound = 0)
     s.problem += lpSum([s.costs[i]*s.variables[i] for i in s.events]), "Total runway time used"   #objective function
-    #s.problem += lpSum([s.costs[i]*s.variables[i] for i in s.ll_events + s.ltl_events + s.lttl_events]), "Total runway time used"
+    #s.problem += lpSum([s.costs[i]*s.variables[i] for i in s.ll_events + s.ltl_events + s.lttl_events]), "Total runway time used"  with added Takeoffs
     s.genconstraints()
-
-
-
-
 
     s.genoutput()
 
 
   def genconstraints(s):
+    """Function to generate the constraints, a string is added to each one of them to describe the constraint textually"""
     for typ in s.types:
       s.problem += lpSum(s.end_begin(-1, typ)) == s.landings[typ], "Number of " + typ + " landed"
       s.problem += lpSum(s.end_begin(-1, typ)) - lpSum(s.end_begin(0, typ)) == 0, "head-tails constraint of " + typ
@@ -35,8 +32,11 @@ class Problem:
   #  problem += lpSum([variables["heavy"], variables["medium"]]) == 1
     #s.problem += lpSum([s.variables["medium->heavy"], s.variables["heavy->medium"]]) >= 1
 
-
+ 
   def gencosts(s):
+    """ Function to generate a dictionary of costs, which are used in the cost function.
+        The costs represent the time a runway is used for a certain event
+        This time is dependent on the seperation based on the aircraft class and the approach speed of the aircraft. """
     costs = {}
     sep=  [[4,5,6],[3,4,5],[3,3,4]]   #seperation vh, H , M * vh, H , M
     v = [150, 140, 120]     # approachspeed kts (make dict!)
@@ -61,6 +61,7 @@ class Problem:
 
 
   def genevents(s):
+    """ Function to generate all possible events based on the list of aircraft types present. """
     events = []
     for typ in s.types:
       events.append("0->"+typ)
@@ -70,6 +71,8 @@ class Problem:
     return events
 
   def genoutput(s):
+    """ Function that writes the problem to an LP file. Following this the problem is solved and the results are printed"""
+    # Might be nice to make a more elegant algorithm
     s.problem.writeLP("Simplified_problem.lp")
     with open('problemout.txt', 'w') as problemout:
       s.problem.solve()
@@ -83,6 +86,7 @@ class Problem:
     print ("done")
 
   def end_begin(s, x, typ):
+    """ List comprehension that is quite often used returning a list with only the events beginning or ending with a certain typ from the entire list"""
     return [s.variables[event] for event in s.events if event.split("->")[x] == typ]
     #return [s.variables[event] for event in s.events + ltl_events + lttl_events if event.split("->")[x] == typ]
 
